7a54f6c77de842a98e0013ca2ffc21e56438ec1d
ZSamuels28 <ZSamuels28@gmail.com>
2022-03-18 14:09:59 -0700

    Added ability to open TXT

--- IMDBParse.py
+++ IMDBParse.py
@@ -5,25 +5,37 @@ import re
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import pandas as pd
-import tkinter
+import tkinter as tk
+#import tkinter
 from tkinter.filedialog import askopenfilename
 import os
 
 MOVIEDB_API_KEY = os.environ.get('MOVIEDB_API_KEY')
 
-MoviesDF = pd.DataFrame(columns=['Title','Input Title','IMDB URL','IMDB Rating','Duration','Genres'])
-TV_ShowsDF = pd.DataFrame(columns=['Title','Input Title','IMDB URL','IMDB Rating','Genres'])
+MoviesDF = pd.DataFrame(columns=['Title','IMDB URL','IMDB Rating','Duration','Genres'])
+TV_ShowsDF = pd.DataFrame(columns=['Title','IMDB URL','IMDB Rating','Genres'])
 Non_IMDB_URLsDF = pd.DataFrame(columns=['URL'])
-ErrorsDF = pd.DataFrame(columns=['Input Name','Input URL'])
+ErrorsDF = pd.DataFrame(columns=['Input URL'])
 #PeopleDF = pd.DataFrame()
 
-print("Please select an HTML file")
+print("Please select an HTML or TXT file")
 
-tkinter.Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+application_window = tk.Tk()
+application_window.withdraw() # we don't want a full GUI, so keep the root window from appearing
+my_filetypes = [('Valid Files', '.txt .html'), ('Text Files', '.txt',), ('HTML Files', '.html')]
+filename = askopenfilename(initialdir=os.getcwd(),title="Please select a file:",filetypes=my_filetypes) # show an "Open" dialog box and return the path to the selected file
 
-with open(filename) as fp:
-    soup = BeautifulSoup(fp, "html.parser")
+if filename.lower().endswith('.html'):
+    with open(filename) as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+        urls = []
+        for i in soup.findAll('a'):
+            urls.append(i.get('href'))
+elif filename.lower().endswith('.txt'):
+    urls = [line.strip() for line in open(filename)]
+else:
+    print("File extension error")
+    exit()
 
 def get_details(id,type):
         query = "https://api.themoviedb.org/3/" + type + "/" + str(id)
@@ -49,8 +61,8 @@ def get_details(id,type):
             return
 
 def write_dictionary(link):
-    if "imdb" in link.get('href'):
-        query = "https://api.themoviedb.org/3/find/tt" + re.findall('\d+',link.get('href'))[0]
+    if "imdb" in link:
+        query = "https://api.themoviedb.org/3/find/tt" + re.findall('\d+',link)[0]
         params = {
             "api_key" : MOVIEDB_API_KEY,
             "language" : "en-US",
@@ -60,23 +72,21 @@ def write_dictionary(link):
         if response.status_code == 200:
             x = json.loads(response.text)
             if not x.get("movie_results") and not x.get("person_results") and not x.get("tv_results"):
-                ErrorsDF.loc[ErrorsDF.shape[0]] = [link.string,link.get('href')]
+                ErrorsDF.loc[ErrorsDF.shape[0]] = [link]
             elif not x.get("person_results") and not x.get("tv_results"):
                 for i in x.get("movie_results"):
                     details = get_details(i.get("id"),"movie")
-                    MoviesDF.loc[MoviesDF.shape[0]] = [i.get("title"),link.string,link.get('href'),str(i.get("vote_average")),details[0],details[1]]
+                    MoviesDF.loc[MoviesDF.shape[0]] = [i.get("title"),link,str(i.get("vote_average")),details[0],details[1]]
             elif not x.get("person_results") and not x.get("movie_results"):
                 for i in x.get("tv_results"):
                     details = get_details(i.get("id"),"tv")
-                    TV_ShowsDF.loc[TV_ShowsDF.shape[0]] = [i.get("name"),link.string,link.get('href'),str(i.get("vote_average")),details]
+                    TV_ShowsDF.loc[TV_ShowsDF.shape[0]] = [i.get("name"),link,str(i.get("vote_average")),details]
             elif not x.get("movie_results") and not x.get("tv_results"):
                 return
         else:
             print("Error")
     else:
-        Non_IMDB_URLsDF.loc[Non_IMDB_URLsDF.shape[0]] = [link.get('href')]
-
-urls = soup.findAll('a')
+        Non_IMDB_URLsDF.loc[Non_IMDB_URLsDF.shape[0]] = [link]
 
 with tqdm(total=len(urls)) as pbar:
     with ThreadPoolExecutor(max_workers=10) as executor:
